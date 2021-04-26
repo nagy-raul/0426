c01e0f189cd14c38dd3a2395c9ef0b24beeb5ca7
Nraul <82936346+nagy-raul@users.noreply.github.com>
2021-04-26 07:51:33 +0200

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,9 +1,62 @@
+let kocka = 0
 let kpo = 0
+input.onButtonPressed(Button.A, function () {
+    kocka = randint(1, 6)
+    if (kocka == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (kocka == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (kocka == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (kocka == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (kocka == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            `)
+    }
+})
 input.onGesture(Gesture.Shake, function () {
-    kpo = randint(0, 2)
-    if (kpo == 0) {
+    kpo = randint(1, 6)
+    if (kpo == 1) {
         basic.showIcon(IconNames.SmallSquare)
-    } else if (kpo == 1) {
+    } else if (kpo == 2) {
         basic.showIcon(IconNames.Square)
     } else {
         basic.showIcon(IconNames.Scissors)
